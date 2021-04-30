21598684d0923a53de95e7a045c7b7519c0d5e35
Vinay Venu <ijkvinay@gmail.com>
2021-04-30 14:24:24 +0530

    Remove dead code

--- src/screens/Routes.js
+++ src/screens/Routes.js
@@ -2,24 +2,12 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
 import BasicDetails from './BasicDetails';
-import {Appbar} from 'react-native-paper';
-
-function CustomNavigationBar({navigation, previous}) {
-  console.log(navigation.state);
-  return (
-    <Appbar.Header>
-      {previous ? <Appbar.BackAction onPress={navigation.goBack}/> : null}
-      <Appbar.Content title="Pneumonia App"/>
-    </Appbar.Header>
-  );
-}
 
 const Stack = createStackNavigator();
 
 export default () => (
-  <Stack.Navigator
-    initialRouteName="Home">
-    <Stack.Screen name="Home" component={Home}/>
-    <Stack.Screen name="BasicDetails" component={BasicDetails}/>
+  <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="BasicDetails" component={BasicDetails} />
   </Stack.Navigator>
 );
