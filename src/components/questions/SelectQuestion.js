1e64e9c457b677a9c33eb86248045ee4680a149e
Arjun <arjunk@thoughtworks.com>
2021-05-03 12:04:38 +0530

    Fixing style issues in Select question

--- src/components/questions/SelectQuestion.js
+++ src/components/questions/SelectQuestion.js
@@ -10,66 +10,65 @@ import QuestionBase from './QuestionBase';
 import messages from '../../domain/messages';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  nonSelectedItem: {
-    backgroundColor: '#F4F5F6',
-    paddingHorizontal: 5,
-    marginVertical: 8,
-    height: 50,
-    borderRadius: 4,
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1
+    },
+    nonSelectedItem: {
+        backgroundColor: '#F4F5F6',
+        paddingHorizontal: 5,
+        marginVertical: 8,
+        minHeight: 50,
+        borderRadius: 4,
+        justifyContent: 'center'
+    },
 
-  selectedItem: {
-    backgroundColor: '#38A160',
-    paddingHorizontal: 5,
-    marginVertical: 8,
-    height: 50,
-    borderRadius: 4,
-    justifyContent: 'center',
-  },
-  selectedAnswerText: {
-    fontSize: 24,
-    color: '#FFF',
-  },
-  nonSelectedAnswerText: {
-    fontSize: 24,
-    color: '#000',
-  },
+    selectedItem: {
+        backgroundColor: '#38A160',
+        paddingHorizontal: 5,
+        marginVertical: 8,
+        minHeight: 50,
+        borderRadius: 4,
+        justifyContent: 'center'
+    },
+    selectedAnswerText: {
+        fontSize: 24,
+        color: '#FFF'
+    },
+    nonSelectedAnswerText: {
+        fontSize: 24,
+        color: '#000'
+    }
 });
 
 const Item = ({title, onPress, selectedAnswers}) => {
-  const isSelected = selectedAnswers.includes(title);
-  return (
-    <TouchableNativeFeedback onPress={onPress}>
-      <View style={[isSelected ? styles.selectedItem : styles.nonSelectedItem]}>
-        <Text
-          style={[
-            isSelected
-              ? styles.selectedAnswerText
-              : styles.nonSelectedAnswerText,
-          ]}>
-          {messages[title]}
-        </Text>
-      </View>
+
+    const isSelected = selectedAnswers.includes(title);
+    return <TouchableNativeFeedback onPress={onPress}>
+        <View style={[isSelected ? styles.selectedItem : styles.nonSelectedItem]}>
+            <Text
+                style={[isSelected ? styles.selectedAnswerText : styles.nonSelectedAnswerText]}>{messages[title]}</Text>
+        </View>
     </TouchableNativeFeedback>
-  );
 };
 
-export default ({number, question, onAnswered = () => {}, value = []}) => {
-  const renderItem = ({item}) => (
-    <Item title={item} onPress={value => onAnswered(question, value)} selectedAnswers={value} />
-  );
+export default ({
+                    number, question, onAnswered = () => {
+    }, selectedAnswers = []
+                }) => {
 
-  return (
-    <QuestionBase number={number} question={question}>
-      <FlatList
-        data={question.options}
-        renderItem={renderItem}
-        keyExtractor={item => item}
-      />
-    </QuestionBase>
-  );
+    const renderItem = ({item}) => (
+        <Item title={item} onPress={onAnswered} selectedAnswers={selectedAnswers}/>
+    );
+
+    return (
+        <View style={styles.container}>
+            <QuestionBase number={number} question={question}/>
+            <FlatList
+                data={question.options}
+                renderItem={renderItem}
+                keyExtractor={item => item}
+            />
+        </View>
+    );
+
 };
--- storybook/stories/Questions/MultiSelectQuestion.stories.js
+++ storybook/stories/Questions/MultiSelectQuestion.stories.js
@@ -3,16 +3,12 @@ import React from 'react';
 import SelectQuestion from '../../../src/components/questions/SelectQuestion';
 import baseScreenDecorator from '../baseScreenDecorator';
 
-storiesOf('MultiSelect Question', module)
-  .add('With Selection', () => (
-    <SelectQuestion
-      number={2}
-      question={{
-        key: 'comorbidities',
-        helpText: 'directlyObserve',
-        options: ['diabetes', 'hypertension', 'heartDisease', 'none'],
-      }}
-      value={['diabetes', 'hypertension']}
-    />
-  ))
-  .addDecorator(baseScreenDecorator);
+storiesOf('MultiSelectQuestion', module)
+    .add('basic view', () => (
+        <SelectQuestion
+            number={2}
+            question={{key: 'comorbidities', helpText: 'directlyObserve', options: ['diabetes', 'hypertension', 'heartDisease', 'asthma', 'lungDisease', 'sickleCellDisease', 'liverDisease','kidneyDisease','pregnancy','obesity','anemia','hiv','cancer','none']}}
+            selectedAnswers={['diabetes', 'hypertension']}
+        />
+    ))
+    .addDecorator(baseScreenDecorator);
