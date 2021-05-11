9f7c0678b52bd6864c28783b80b44cc01f59d54c
Vinay Venu <ijkvinay@gmail.com>
2021-05-11 14:53:03 +0530

    Add key to looping view

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -11,21 +11,29 @@ export default ({form = {}}) => {
   const [showAnswers, setShowAnswers] = useState(true);
 
   const questions = visibleQuestions(form);
-  const answers = _.reduce(questions, (result, question) => {
-    if (question.type === questionTypes.information) {
-      result.push({question: t(question.key), value: question.value(form), output: question.output(form).weight});
+  const answers = _.reduce(
+    questions,
+    (result, question) => {
+      if (question.type === questionTypes.information) {
+        result.push({
+          question: t(question.key),
+          value: question.value(form),
+          output: question.output(form).weight,
+        });
+        return result;
+      }
+      const value = form[question.key];
+      if (!_.isNil(value)) {
+        result.push({
+          question: t(question.key),
+          value: getAnswerString(question, value),
+          output: question.output(form).weight,
+        });
+      }
       return result;
-    }
-    const value = form[question.key];
-    if (!_.isNil(value)) {
-      result.push({
-        question: t(question.key),
-        value: getAnswerString(question, value),
-        output: question.output(form).weight
-      });
-    }
-    return result;
-  }, []);
+    },
+    [],
+  );
 
   return (
     <View>
@@ -36,34 +44,45 @@ export default ({form = {}}) => {
       {/*        color={colors.accent}*/}
       {/*        onPress={() => setShowAnswers(!showAnswers)}>{showAnswers ? t('hideAnswers') : t('showAnswers')}</Button>*/}
       {showAnswers && (
-        <View style={{
-          marginHorizontal: 8,
-          marginVertical: 16,
-          padding: 15,
-          backgroundColor: '#EEF0F2',
-          elevation: 2,
-          borderRadius: 8,
-        }}>
+        <View
+          style={{
+            marginHorizontal: 8,
+            marginVertical: 16,
+            padding: 15,
+            backgroundColor: '#EEF0F2',
+            elevation: 2,
+            borderRadius: 8,
+          }}>
           <Text style={{textAlign: 'center', fontSize: 32}}>
             {t('answers')}
           </Text>
           {answers.map(answer => {
             return (
-              <View style={{flexDirection: 'row', marginVertical: 8}}>
+              <View
+                key={answer.question}
+                style={{flexDirection: 'row', marginVertical: 8}}>
                 <View style={{width: 48}}>
                   <View
-                    style={{width: 36, height: 36, borderRadius: 18, backgroundColor: colors[answer.output]}}></View>
+                    style={{
+                      width: 36,
+                      height: 36,
+                      borderRadius: 18,
+                      backgroundColor: colors[answer.output],
+                    }}></View>
                 </View>
                 <View style={{minHeight: 24, flex: 1}}>
-                  <Text style={{fontSize: 18, color: colors.primary}}>{answer.question}</Text>
-                  <Text style={{fontSize: 18, color: colors.accent}}>{answer.value}</Text>
+                  <Text style={{fontSize: 18, color: colors.primary}}>
+                    {answer.question}
+                  </Text>
+                  <Text style={{fontSize: 18, color: colors.accent}}>
+                    {answer.value}
+                  </Text>
                 </View>
               </View>
-            )
+            );
           })}
         </View>
-      )
-      }
+      )}
     </View>
-  )
-}
+  );
+};
