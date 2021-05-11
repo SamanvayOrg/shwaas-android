80a4df2f4d53d16c62a89493955c407bbcc255fb
Vinay Venu <ijkvinay@gmail.com>
2021-05-11 13:41:11 +0530

    Do not add analytics in dev mode

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,8 @@
 import Analytics from 'appcenter-analytics';
 
 const trackRecommendation = recommendation => {
+  if (__DEV__) return;
+
   Analytics.trackEvent('Recommendation given', {
     recommendation: recommendation.nextSteps,
   });
